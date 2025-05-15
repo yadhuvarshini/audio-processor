b8909db5455efcfcc3ce4a958929963e144aa224
Yadhuvarshini <yadhuvarshini1@gmail.com>
2025-05-15 18:48:35 +0530

    pipelines of channel for each stage

---
+++ pipeline/manager.go
@@ -0,0 +1,25 @@
+package pipeline
+
+import (
+
+	"context"  //concurrency control and exit
+	"github.com/yadhuvarshini/audio-processsor/model"
+	
+	)
+type Pipeline struct {
+	IngestChan chan model.AudioChunk
+	ValidateChan chan model.AudioChunk
+	TransformChan chan model.AudioChunk
+	ExtractChan chan model.AudioChunk
+	StorageChan chan model.AudioChunk
+}
+
+func NewPipeline(ctx context.Context) *Pipeline {
+	return &Pipeline{
+		IngestChan:   make(chan model.AudioChunk, 100),
+		ValidateChan: make(chan model.AudioChunk, 100),
+		TransformChan: make(chan model.AudioChunk, 100),
+		ExtractChan:  make(chan model.AudioChunk, 100),
+		StorageChan:  make(chan model.AudioChunk, 100),
+	}
+}
